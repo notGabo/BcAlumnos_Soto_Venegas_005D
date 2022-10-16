1a6370385be4ac50d7e2ef8f27f7a5d61b57650c
notGabo <ga.soto@duocuc.cl>
2022-10-16 15:20:47 -0400

    avanzo 1

--- app/pages/escanear-qr/escanear-qr.page.ts
+++ app/pages/escanear-qr/escanear-qr.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-escanear-qr',
@@ -10,7 +10,35 @@ export class EscanearQrPage implements OnInit {
 
   constructor() { }
 
+  public modalAbierto = false;
+
   ngOnInit() {
   }
 
+
+
+  public async startScan() {
+    const element = document.getElementById('fondoEsp');
+    const boton = document.getElementById('boton');
+    await BarcodeScanner.checkPermission({ force: true });
+    BarcodeScanner.hideBackground();
+    element.classList.add('fondoInvisible');
+    element.classList.remove('fondo');
+    boton.style.visibility = 'hidden';
+    const result = await BarcodeScanner.startScan();
+    if (result.hasContent) {
+      this.modalAbierto = true;
+      BarcodeScanner.showBackground();
+      element.classList.remove('fondoInvisible');
+      element.classList.add('fondo');
+      boton.style.visibility = 'visible';
+    }
+  }
+
+  public abrir(input: boolean) {
+    this.modalAbierto = input;
+  }
+
 }
+
+
